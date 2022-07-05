458be9134e867040f79b47458819d63d176c002b
jimmy.wang <jimmy.wang@oocl.com>
2022-07-05 14:16:09 +0800

    array homework

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
 //TODO
-
+//a no, b yes
 
 // Write a program that multiplies each entry in the following array by 2。
 var a = [1, 2, 3, 4, 5];
 // TODO should output [2,4,6,8,10]
+for (i = 0; i < a.length; i++) {
+    a[i] *= 2;
+}
 
 
 // Write the program, according to the following requirements output results.
@@ -14,13 +17,42 @@ var colors = ["Red", "Green", "White", "Black"];
 //TODO case 1 output: 'Red Green White Black'
 // case 2 output: 'Red+Green+White+Black'
 // case 3 output: 'Red,Green,White,Black'
+var num = 0;
+switch (num) {
+    case 1:
+        console.log(colors.join(' '));
+        break;
+    case 2:
+        console.log(colors.join('+'));
+        break;
+    case 3:
+        console.log(colors.join(','));
+        break;
+    default:
+        console.log('input num:1-3');
+}
 
 
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
+a.sort(function (a, b) {
+    return b - a;
+})
 
 
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 //TODO should output: 'a'
+var obj = {};
+a.forEach(i => {
+   obj[i] = obj[i] ? obj[i]+1 : 1
+});
+var max = 0, key = -1;
+for (let i in obj) {
+    if (max < obj[i]){
+        max = obj[i];
+        key = i;
+    }
+}
+console.log(key);
